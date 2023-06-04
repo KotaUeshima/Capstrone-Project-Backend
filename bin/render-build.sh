c7db6c6a9c7dc0842c11ce2f6baeb6135d993979
Kota Ueshima <ueshima@usc.edu>
2023-06-04 00:15:31 -0500

    try to fix deploy issues

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+bin/rails db:migrate
+
 bundle install
 # bundle exec rails assets:precompile
 # bundle exec rails assets:clean
-bundle exec rails db:seed db:migrate
+bundle exec rails db:migrate db:seed
